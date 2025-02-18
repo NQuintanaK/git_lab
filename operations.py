aa441f23bb9ad1585db1c6f9e9fe78e155d01ac0
jinsik <a01026630@tec.mx>
2025-02-18 08:57:03 -0600

    Added Addition and Subtraction.

--- operations.py
+++ operations.py
@@ -3,7 +3,9 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
+console.log("Addition: ", a + b)
 
 # Display the subtraction
+console.log("Subtraction: ", a - b)
 
 # Display multiplication
